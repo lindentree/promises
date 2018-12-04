369f89a2ae5e8c9d8fdb26114f30b49a08156bdb
lindentree <hchiu87@gmail.com>
2018-12-03 19:46:09 -0800

    Implement promisification

--- exercises/bare_minimum/promisification.js
+++ exercises/bare_minimum/promisification.js
@@ -27,7 +27,29 @@ var getGitHubProfile = function(user, callback) {
   });
 };
 
-var getGitHubProfileAsync; // TODO
+var getGitHubProfileAsync = function(user, callback) {
+  var promise = new Promise((resolve, reject) => {
+    var options = {
+      url: 'https://api.github.com/users/' + user,
+      headers: { 'User-Agent': 'request' },
+      json: true  // will JSON.parse(body) for us
+    };
+
+    request.get(options, function(err, res, body) {
+      if (err) {
+        reject(err);
+      } else if (body.message) {
+        reject(new Error('Failed to get GitHub profile: ' + body.message));
+      } else {
+        resolve(body);
+      }
+    });
+
+  });
+
+  return promise;
+
+};
 
 
 // (2) Asyncronous token generation
@@ -38,7 +60,7 @@ var generateRandomToken = function(callback) {
   });
 };
 
-var generateRandomTokenAsync; // TODO
+var generateRandomTokenAsync = Promise.promisify(generateRandomToken); // TODO
 
 
 // (3) Asyncronous file manipulation
@@ -56,7 +78,26 @@ var readFileAndMakeItFunny = function(filePath, callback) {
   });
 };
 
-var readFileAndMakeItFunnyAsync; // TODO
+var readFileAndMakeItFunnyAsync = function(filePath, callback) {
+  var promise = new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', function(err, file) {
+      if (err) { 
+        reject(err); 
+      } else {
+        var funnyFile = file.split('\n')
+          .map(function(line) {
+            return line + ' lol';
+          })
+          .join('\n');
+
+        resolve(funnyFile);
+      }
+    });
+  });
+
+  return promise;
+
+}; 
 
 // Export these functions so we can test them and reuse them in later exercises
 module.exports = {
